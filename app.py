9303f83bb2d85521d0b0c8d5c105d1c585840618
Gabriel Allen <gabriel.houston.allen@gmail.com>
2021-04-22 20:32:10 -0600

    update flask

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-
-
 # Import our modules
 import requests
 from pandas.io.json import json_normalize
@@ -26,17 +24,6 @@ db = client.charging_stationDB
 @app.route("/")
 def Index():
     return render_template('index.html')
-# def home():
-#     return (
-#         f"Welcome to the Charging Stations DataBase!<br/>"
-#         f"<br/>"
-#         f"Below route returns all Charging Stations from database<br/>"
-#         f"/api/v1.0/Charging_Stations<br/>"
-#         f"<br/>"
-#         f"Below route returns all Charging Stations from database<br/>"
-#         f"/api/v1.0/City_Charging_Stations<br/>"
-#         f"<br/>"
-#     )
 
 
 @app.route("/api/v1.0/Charging_Stations")
@@ -44,33 +31,6 @@ def all_stations():
     # Store collection in a list
     Charging_Station_Records = db.charging_stations.find()
 
-    # Create empty list and fill with collection records
-    # data = []
-
-    # for item in Charging_Station_Records:
-    #     charging_stations_dict = {}
-    #     try:
-    #         charging_stations_dict['fuel_type_code'] = item['fuel_type_code']
-    #         charging_stations_dict['station_name'] = item['station_name']
-    #         charging_stations_dict['street_address'] = item['street_address']
-    #         charging_stations_dict['state'] = item['state']
-    #         charging_stations_dict['latitude'] = item['latitude']
-    #         charging_stations_dict['longitude'] = item['longitude']
-    #         charging_stations_dict['station_phone'] = item['station_phone']
-    #         charging_stations_dict['status_code'] = item['status_code']
-    #         charging_stations_dict['groups_with_access_code'] = item['groups_with_access_code']
-    #         charging_stations_dict['id'] = item['id']
-    #         charging_stations_dict['owner_type_code'] = item['owner_type_code']
-    #         charging_stations_dict['Zip Code'] = item['Zip Code']
-    #         charging_stations_dict['City'] = item['City']
-    #         charging_stations_dict['County'] = item['County']
-    #         charging_stations_dict['Population'] = item['Population']
-    #         charging_stations_dict['Average Household Income'] = item['Average Household Income']
-    #         data.append(charging_stations_dict)
-    #     except:
-    #         pass
-
-#     return jsonify({'data': data[0:2]})
     data = []
     for item in Charging_Station_Records:
         item_dict = {}
